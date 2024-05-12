f0a2c7871ebbe396f17a813c4a821dc90a8a8baa
Musaab Almuqbali <almuqbalimusab@gmail.com>
2024-05-12 23:06:59 +0400

    migrating to aws-sdk v3 from v2

--- server/controllers/abstractController.js
+++ server/controllers/abstractController.js
@@ -1,18 +1,83 @@
 import { abstractsCollection } from "../models/abstractModel.js";
 import { validateEmail } from "../helpers/validateEmail.js";
 import dotenv from 'dotenv';
-import AWS from 'aws-sdk';
-import fs from 'fs';
+// import AWS from 'aws-sdk';
+import { GetObjectCommand, S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import fs  from 'fs';
 import util from 'util';
-
 dotenv.config();
 const unlinkFile = util.promisify(fs.unlink); // Promisify the unlink function
 
-const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_S3_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_S3_SECRET_KEY,
-    region: process.env.AWS_S3_REGION
+// const s3 = new AWS.S3({
+//     accessKeyId: process.env.AWS_S3_ACCESS_KEY,
+//     secretAccessKey: process.env.AWS_S3_SECRET_KEY,
+//     region: process.env.AWS_S3_BUCKET_REGION,
+// });
+
+const s3Client = new S3Client({
+    region: process.env.AWS_S3_BUCKET_REGION, // Make sure this environment variable is set
+    credentials: {
+        accessKeyId: process.env.AWS_S3_ACCESS_KEY,  // Ensure these are set
+        secretAccessKey: process.env.AWS_S3_SECRET_KEY
+    }
 });
+
+
+// export const submitAbstract = async (req, res) => {
+//     const { firstName, lastName, email, mobile, topic } = req.body;
+
+//     if (!req.file) {
+//         return res.status(401).json({ message: 'Please upload your abstract file' });
+//     }
+//     const { originalname, path } = req.file;
+
+//     if (!email || !mobile || !topic) {
+//         return res.status(401).json({ message: 'Please enter all required fields' });
+//     }
+//     if (!validateEmail(email)) {
+//         return res.status(401).json({ message: 'Invalid email address' });
+//     }
+//     if (!originalname) {
+//         return res.status(401).json({ message: 'Please upload your abstract file' });
+//     }
+
+//     const uploadParams = {
+//         Bucket: process.env.AWS_S3_BUCKET_NAME,
+//         Key: originalname,
+//         Body: fs.createReadStream(path)
+//     };
+
+//     try {
+//         const uploadResult = await s3.upload(uploadParams).promise();
+//         console.log('File uploaded successfully:', uploadResult.Location);
+//         await unlinkFile(path); // Delete the file safely after upload
+
+//         const newAbstract = {
+//             firstName,
+//             lastName,
+//             email: email.toLowerCase(),
+//             phoneNo: mobile,
+//             fileName: originalname,
+//             topic: topic,
+//             created_at: new Date()
+//         };
+
+//         await abstractsCollection.insertOne(newAbstract);
+//         return res.status(201).json({ message: 'Abstract submitted successfully' });
+
+//     } catch (error) {
+//         console.error('Error:', error);
+//         await unlinkFile(path); // Ensure the file is deleted even if an error occurs
+//         if (error.code === 'NoSuchBucket') {
+//             return res.status(500).json({ message: 'Bucket does not exist' });
+//         }
+//         if (error.code === 11000) {
+//             return res.status(400).json({ message: 'File name already used' });
+//         }
+//         return res.status(500).json({ message: 'Something went wrong' });
+//     }
+// };
+
 
 export const submitAbstract = async (req, res) => {
     const { firstName, lastName, email, mobile, topic } = req.body;
@@ -20,6 +85,7 @@ export const submitAbstract = async (req, res) => {
     if (!req.file) {
         return res.status(401).json({ message: 'Please upload your abstract file' });
     }
+
     const { originalname, path } = req.file;
 
     if (!email || !mobile || !topic) {
@@ -39,7 +105,8 @@ export const submitAbstract = async (req, res) => {
     };
 
     try {
-        const uploadResult = await s3.upload(uploadParams).promise();
+        const command = new PutObjectCommand(uploadParams);
+        const uploadResult = await s3Client.send(command);
         console.log('File uploaded successfully:', uploadResult.Location);
         await unlinkFile(path); // Delete the file safely after upload
 
@@ -59,7 +126,7 @@ export const submitAbstract = async (req, res) => {
     } catch (error) {
         console.error('Error:', error);
         await unlinkFile(path); // Ensure the file is deleted even if an error occurs
-        if (error.code === 'NoSuchBucket') {
+        if (error.name === 'NoSuchBucket') {
             return res.status(500).json({ message: 'Bucket does not exist' });
         }
         if (error.code === 11000) {
@@ -70,23 +137,47 @@ export const submitAbstract = async (req, res) => {
 };
 
 export const downloadSpesificAbstract = async (req, res) => {
-    console.log('req coming');
+    // console.log('req coming');
+    // const key = req.params.key;
+    // const downloadParams = {
+    //     Bucket: process.env.AWS_S3_BUCKET_NAME,
+    //     Key: key
+    // };
+
+    // s3.getObject(downloadParams, (err, data) => {
+    //     if (err) {
+    //         console.error('Error downloading file:', err);
+    //         return res.status(500).send('Internal Server Error');
+    //     }
+    //     res.set({
+    //         'Content-Type': data.ContentType,
+    //         'Content-Length': data.ContentLength,
+    //         'Content-Disposition': `attachment; filename="${key}"`
+    //     });
+    //     res.send(data.Body);
+    // });  
+
     const key = req.params.key;
     const downloadParams = {
         Bucket: process.env.AWS_S3_BUCKET_NAME,
         Key: key
     };
 
-    s3.getObject(downloadParams, (err, data) => {
-        if (err) {
-            console.error('Error downloading file:', err);
-            return res.status(500).send('Internal Server Error');
-        }
+    try {
+        const command = new GetObjectCommand(downloadParams);
+        const { ContentType, ContentLength, Body } = await s3Client.send(command);
+
         res.set({
-            'Content-Type': data.ContentType,
-            'Content-Length': data.ContentLength,
+            'Content-Type': ContentType,
+            'Content-Length': ContentLength,
             'Content-Disposition': `attachment; filename="${key}"`
         });
-        res.send(data.Body);
-    });  
+        Body.pipe(res);
+    } catch (err) {
+        console.error('Error downloading file:', err);
+        if (err.name === 'NoSuchKey') {
+            return res.status(404).send('File not found');
+        }
+        return res.status(500).send('Internal Server Error');
+    }
 };
