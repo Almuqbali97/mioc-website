d487e1418c11b42814fcd224bd06a8d438c8b5b3
Musaab Almuqbali <almuqbalimusab@gmail.com>
2024-05-22 17:16:42 +0400

    test vercel

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 import express from 'express';
-import { connectDB } from './config/connectMongodb.js'; // Update path as needed
+// import { connectDB } from './config/connectMongodb.js'; // Update path as needed
 import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import userRoutes from './routes/userRoutes.js';
-import abstractRoutes from './routes/abstractRoutes.js';
-import { isAuthenticated } from './middlewares/isAuenticatedMiddleware.js';
-import { isAdmin } from './middlewares/isAuthorizedAdmin.js';
-import ticketRoutes from './routes/ticketPaymentRoutes.js'
-import registrants from './routes/registrantsRoutes.js'
+// import cookieParser from 'cookie-parser';
+// import userRoutes from './routes/userRoutes.js';
+// import abstractRoutes from './routes/abstractRoutes.js';
+// import { isAuthenticated } from './middlewares/isAuenticatedMiddleware.js';
+// import { isAdmin } from './middlewares/isAuthorizedAdmin.js';
+// import ticketRoutes from './routes/ticketPaymentRoutes.js'
+// import registrants from './routes/registrantsRoutes.js'
 const app = express();
 const port = process.env.PORT ||3000;
 
@@ -37,16 +37,17 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     res.json("Hello from api")
 })
-//routes
-app.use('/user', userRoutes);
-app.use('/abstract', abstractRoutes)
-app.use('/', ticketRoutes)
-app.use('/', registrants)
+    //routes
+    
+// app.use('/user', userRoutes);
+// app.use('/abstract', abstractRoutes)
+// app.use('/', ticketRoutes)
+// app.use('/', registrants)
 
-// testing admin auth
-app.get('/admin', isAuthenticated, isAdmin, (req, res) => {
-    res.json({ user: req.user });
-});
+// // testing admin auth
+// app.get('/admin', isAuthenticated, isAdmin, (req, res) => {
+//     res.json({ user: req.user });
+// });
 
 (async () => {
     await connectDB();
