76aea6b03477749239dd491b14aa075f5fa8b1d9
Musaab Almuqbali <almuqbalimusab@gmail.com>
2024-06-03 17:30:02 +0400

    conference registration details url edited

--- client/src/components/website/ProfileDropdown.jsx
+++ client/src/components/website/ProfileDropdown.jsx
@@ -104,9 +104,9 @@ const ProfileDropdown = () => {
                                     d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09z"
                                 />
                             </svg>
-                            <a href="#" onClick={() => handleOptionClick()}>
+                            <Link to={'/user/conference/registration'} onClick={() => handleOptionClick()}>
                                 Conference Registration
-                            </a>
+                            </Link>
                         </div>
                         <div className="flex gap-2 items-center mt-4">
                             <svg
