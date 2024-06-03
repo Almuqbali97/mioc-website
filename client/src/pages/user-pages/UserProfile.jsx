03db3762da6f4409fbb4fc15b6ac89031e7f8e80
Musaab Almuqbali <almuqbalimusab@gmail.com>
2024-06-03 19:10:30 +0400

    update fetch profile test2

--- client/src/pages/user-pages/UserProfile.jsx
+++ client/src/pages/user-pages/UserProfile.jsx
@@ -37,7 +37,7 @@ const UserProfile = ({ onClose }) => {
             <h1 className="text-3xl text-primary_blue text-center font-bold mt-8 mb-6">User Profile</h1>
             <img className='h-20 w-20 rounded-full absolute -top-12 left-1/2 transform -translate-x-1/2 -translate-y-1/2'
                 src='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png' />
-            {isLogin && Object.keys(userProfileData).length > 0 ? (
+            {isLogin && Object.keys(user).length > 0 ? (
                 <div className="bg-white shadow-md rounded-lg p-6 w-full ">
                     <div className="mb-4">
                         <h2 className="text-xl font-semibold">Profile Information</h2>
