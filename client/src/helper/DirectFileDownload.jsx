6eb3a2d5df931c12ba30bef14f292cbe424de92b
Musaab Almuqbali <almuqbalimusab@gmail.com>
2024-06-03 22:06:22 +0400

    delete un wanted page

--- client/src/helper/DirectFileDownload.jsx
+++ client/src/helper/DirectFileDownload.jsx
@@ -44,7 +44,7 @@ const DirectFileDownload = ({ fileKey, btnText }) => {
     return (
         <div>
             <button
-                onClick={fetchAndDownloadFile}
+                onMouseDown={fetchAndDownloadFile}
                 className='inline-block sm:text-md font-semibold w-auto text-center min-w-[180px] px-1 py-3 sm:px-[1.20rem] sm:py-4 text-white transition-all rounded-md shadow-lg sm:w-auto bg-gradient-to-r from-yellow-600 to-primary_brown hover:bg-gradient-to-b dark:shadow-blue-900 shadow-amber-300 hover:shadow-xl hover:shadow-primary_brown hover:-tranneutral-y-px '
                 disabled={loading}
             >
