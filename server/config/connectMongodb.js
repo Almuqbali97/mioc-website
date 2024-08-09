2f5be6e71de0c7fb9c2df200fd4df3335d1e9b0c
Musaab Almuqbali <almuqbalimusab@gmail.com>
2024-08-09 18:59:45 +0400

    update mongodb connection

--- server/config/connectMongodb.js
+++ server/config/connectMongodb.js
@@ -1,8 +1,54 @@
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import dotenv from 'dotenv';
+dotenv.config();
+// // database setup
+const mongoDBURL = process.env.MONGODB_URL;
+// creating new client
+const mongoClient = new MongoClient(mongoDBURL, {
+    family: 4,
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+    connectTimeoutMS: 30000, // 30 seconds
+    socketTimeoutMS: 10000, // 10 seconds
+});
+
+// creating db
+
+export async function connectDB() {
+    try {
+        await mongoClient.connect();
+        console.log('mongodb connected');
+    } catch (error) {
+        console.error('Failed to connect to MongoDB', error);
+        process.exit(1);
+    };
+}
+
+export const db = mongoClient.db(process.env.DB_NAME);
+
+async function closeDB() {
+    try {
+        await mongoClient.close();
+        console.log('MongoDB connection closed');
+    } catch (error) {
+        console.error('Error closing MongoDB connection', error);
+    }
+}
+
+// Close connection gracefully
+// process.on('SIGINT', closeDB);
+// process.on('SIGTERM', closeDB);
+
 // import { MongoClient, ServerApiVersion } from 'mongodb';
 // import dotenv from 'dotenv';
 // dotenv.config();
+
 // // database setup
 // const mongoDBURL = process.env.MONGODB_URL;
+
 // // creating new client
 // const mongoClient = new MongoClient(mongoDBURL, {
 //     family: 4,
@@ -10,7 +56,9 @@
 //         version: ServerApiVersion.v1,
 //         strict: true,
 //         deprecationErrors: true,
-//     }
+//     },
+//     connectTimeoutMS: 30000, // 30 seconds
+//     socketTimeoutMS: 10000, // 10 seconds
 // });
 
 // // creating db
@@ -23,7 +71,7 @@
 //     } catch (error) {
 //         console.error('Failed to connect to MongoDB', error);
 //         process.exit(1);
-//     };
+//     }
 // }
 
 // async function closeDB() {
@@ -38,51 +86,38 @@
 // // Close connection gracefully
 // process.on('SIGINT', closeDB);
 // process.on('SIGTERM', closeDB);
-// export { db, connectDB }
 
-import { MongoClient, ServerApiVersion } from 'mongodb';
-import dotenv from 'dotenv';
-dotenv.config();
+// export { db, connectDB };
 
-// database setup
-const mongoDBURL = process.env.MONGODB_URL;
+// import { MongoClient, ServerApiVersion } from "mongodb";
 
-// creating new client
-const mongoClient = new MongoClient(mongoDBURL, {
-    family: 4,
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    },
-    connectTimeoutMS: 30000, // 30 seconds
-    socketTimeoutMS: 10000, // 10 seconds
-});
+// // Replace the placeholder with your Atlas connection string
+// const uri = process.env.MONGODB_URL;
 
-// creating db
-const db = mongoClient.db(process.env.DB_NAME);
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//     serverApi: {
+//         version: ServerApiVersion.v1,
+//         strict: true,
+//         deprecationErrors: true,
+//     }
+// }
+// );
 
-async function connectDB() {
-    try {
-        await mongoClient.connect();
-        console.log('mongodb connected');
-    } catch (error) {
-        console.error('Failed to connect to MongoDB', error);
-        process.exit(1);
-    }
-}
+// export async function connectDB() {
+//     try {
+//         // Connect the client to the server (optional starting in v4.7)
+//         await client.connect();
 
-async function closeDB() {
-    try {
-        await mongoClient.close();
-        console.log('MongoDB connection closed');
-    } catch (error) {
-        console.error('Error closing MongoDB connection', error);
-    }
-}
+//         // Send a ping to confirm a successful connection
+//         await client.db(process.env.DB_NAME).command({ ping: 1 });
+//         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//     } finally {
+//         // Ensures that the client will close when you finish/error
+//         await client.close();
+//     }
+// }
 
-// Close connection gracefully
-process.on('SIGINT', closeDB);
-process.on('SIGTERM', closeDB);
 
-export { db, connectDB };
+
+
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,5 +1,6 @@
-import { db } from "../config/connectMongodb.js";
+import { db, connectDB } from "../config/connectMongodb.js";
 // creating collection with schema
+// await connectDB();
 export const usersCollection = db.collection('users');
 // making sure the emeil or username is unique
 usersCollection.createIndex({ "email": 1 }, { unique: true });
