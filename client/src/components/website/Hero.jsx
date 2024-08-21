21d81a6c573d8a539f30aa0f6e9d9971a28a7ab5
Musaab Almuqbali <almuqbalimusab@gmail.com>
2024-08-21 08:00:50 +0400

    hero image optimized

--- client/src/components/website/Hero.jsx
+++ client/src/components/website/Hero.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import HeroImg1 from '../../assets/images/hero1.jpg';
-import Heroex1 from '../../assets/images/heroex1.jpg';
+import Heroex1 from '../../assets/images/heroex1compressed.jpg';
 // import HeroImg2 from '../../assets/images/hero2.jpg';
 // import HeroImg3 from '../../assets/images/hero3.jpg';
 // import HeroImg4 from '../../assets/images/hero4.jpg';
